0a9c99d058040d082d5cc4e6c82ad7b61a4f0180
bijick <biji@intellisense.academy>
2024-01-05 17:28:01 +0530

    My new commit for object repository

---
+++ src/main/java/ObjectRepo/LoginPage.java
@@ -0,0 +1,11 @@
+package ObjectRepo;
+
+public class LoginPage {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This object repository will contain the all element from login page");
+
+	}
+
+}
